3295b51ec75eea1c79cbaabcb8a5dba440edd9b1
yolov.detelin <yolov.detelin@students.softuni.bg>
2023-08-12 13:30:46 +0300

    fix ui test

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('http://yolov-detelin.onrender.com');
+    await page.goto('http://yolov-detelin.onrender.com/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
